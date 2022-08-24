2c12c3a88c8862626191b6b358959a04e0fc23c0
ksy7461 <sask89@naver.com>
2022-08-24 13:38:08 +0900

    0824 실습

--- 02_Shooting/Assets/Scripts/Move.cs
+++ 02_Shooting/Assets/Scripts/Move.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
@@ -22,7 +23,6 @@ public class Move : MonoBehaviour
     /// </summary>
     private void Update()
     {
-
         //Vector3 : 백터를 표현하기 위한 구조체. 위치를 표현할 때도 많이 사용한다.  
         //백터 : 힘의 방향과 크기를 나타내는 단 위 
 
@@ -38,6 +38,38 @@ public class Move : MonoBehaviour
         //transform.position += (speed * Time.deltaTime * Vector3.down); // 아래쪽 방향으로 speed만큼 움직여라 (매 초마다)
 
 
+        //TestOldInputManager();
+
+        //transform.position += (speed * Time.deltaTime * dir);
+
+        // Input System
+        // Event-driven(이벤트 드리븐) 방식으로 구현 -> 일이 있을 때만 동작한다.(전력을 아끼기에 적합한 구조)
+
+    }
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        if (context.started) // 매핑된 키가 누른 직후 
+        {
+            Debug.Log("입력들어옴 - started");
+        }
+        if (context.performed) // 매핑된 키가 확실하게 눌려졌다.
+        {
+            Debug.Log("입력들어옴 - performed");
+        }
+        if (context.canceled) //매핑된 키가 떨어졌을 때.
+        {
+            Debug.Log("입력들어옴 - canceled");
+        }
+        Vector2 inputDir = context.ReadValue<Vector2>();
+        Debug.Log(inputDir);
+    }
+
+    /// <summary>
+    /// 인풋 매니저 사용 예시
+    /// </summary>
+    private void TestOldInputManager()
+    {
         //Input Manager을 이용한 입력 처리
         //Busy wait이 발생 (하는일은 없지만 사용되고 있는 상태 => 전력 세이빙을 방해 => 전력 커짐)
         if (Input.GetKeyDown(KeyCode.W))
@@ -60,11 +92,5 @@ public class Move : MonoBehaviour
             Debug.Log("D가 눌러졌다.");
             dir = Vector3.right;
         }
-
-        transform.position += (speed * Time.deltaTime * dir);
-
-        // Input System
-        // Event-driven(이벤트 드리븐) 방식으로 구현 -> 일이 있을 때만 동작한다.(전력을 아끼기에 적합한 구조)
-
     }
 }
